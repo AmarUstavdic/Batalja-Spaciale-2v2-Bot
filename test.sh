13c79f87ebae3eeea656f26960f1d4550127d9e2
lilwizzz <amar.ustavdija@gmail.com>
2024-01-04 13:38:20 +0100

    updated test script and included file structure alongside game evaluator

--- test.sh
+++ test.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-export DISPLAY=:0.0
+BASE_DIR=$(dirname "$0")
+SRC_DIR="$BASE_DIR/src"
+GAME_DIR="$BASE_DIR/Game"
+P1_DIR="$GAME_DIR/p1"
+P2_DIR="$GAME_DIR/p2"
+P3_DIR="$GAME_DIR/p3"
+P4_DIR="$GAME_DIR/p4"
 
-rm -rf Game/p1/*
-cp src/* Game/p1/
-javac Game/p1/*
+compile_and_copy() {
+    # shellcheck disable=SC2115
+    rm -rf "$1"/*
+    cp "$SRC_DIR"/* "$1"/
+    javac "$1"/*
+}
 
-rm -rf Game/p2/*
-cp src/* Game/p2/
-javac Game/p2/*
+compile_and_copy "$P1_DIR"
+compile_and_copy "$P2_DIR"
 
-rm -rf Game/p3/*
-rm -rf Game/p4/*
+# shellcheck disable=SC2115
+rm -rf "$P3_DIR"/*
+cp "$GAME_DIR/Player.java" "$P3_DIR"/
+javac "$P3_DIR"/*
 
-cp Game/Player.java Game/p3/
-cp Game/Player.java Game/p4/
+# shellcheck disable=SC2115
+rm -rf "$P4_DIR"/*
+cp "$GAME_DIR/Player.java" "$P4_DIR"/
+javac "$P4_DIR"/*
 
-javac Game/p3/*
-javac Game/p4/*
-
-java -jar Game/Game.jar Game/p1 Game/p2 Game/p3 Game/p4
-
+# Run the game
+java -jar "$GAME_DIR/Game.jar" "$P1_DIR" "$P2_DIR" "$P3_DIR" "$P4_DIR"
